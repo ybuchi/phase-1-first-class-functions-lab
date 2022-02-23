3ced64e50af1613210e746b2e839e2fdb2f46d2d
ybuchi <yabuch_ch@hotmail.com>
2022-02-23 13:23:37 -0500

    commit tree 88c62b56516af655d4003530353a0c87bcb7d506

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(array){
+    return array.slice(0,2);
+};
+
+const returnLastTwoDrivers = function(array){
+    return array.slice(Math.max(array.length - 2,1));
+};
+
+function createFareMultiplier(multiplier){
+    return function(fare){return fare * multiplier}; 
+};
+
+const fareDoubler = function(fare){
+    return fare * 2;
+};
+
+const fareTripler = function(fare){
+    return fare * 3;
+};
+
+function selectDifferentDrivers(driverArray, func){
+    if(func === returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(driverArray);
+    }else if(func === returnLastTwoDrivers){
+        return returnLastTwoDrivers(driverArray);
+    }
+};
+
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
